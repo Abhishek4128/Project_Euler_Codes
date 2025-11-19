affddf6c1b9182226979d5ace5ce09bdfdd2a362
Abhishek4128 <abhishekmandal0204@gmail.com>
2025-11-19 20:25:30 +0530

    i am adding code for largest prime factor

---
+++ 03_Project_Euler_Largest_Prime_factor.py
@@ -0,0 +1,13 @@
+
+# The prime factors of 13195 are 5, 7, 13, and 29.
+
+# What is the largest prime factor of the number 600851475143?
+
+num=600851475143
+factor=2
+while num>1:
+    if num%factor==0:
+        num=num//factor
+    else:
+        factor=factor+1
+print(factor)
